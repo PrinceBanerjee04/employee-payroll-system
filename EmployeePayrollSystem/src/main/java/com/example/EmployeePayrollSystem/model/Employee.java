95c9cadf731e476b1a4f5d41bbda63f43876ce10
Prince <banerjee.prince69@gmail.com>
2024-08-24 21:04:42 +0530

    Employee Data_Model

--- EmployeePayrollSystem/src/main/java/com/example/EmployeePayrollSystem/model/Employee.java
+++ EmployeePayrollSystem/src/main/java/com/example/EmployeePayrollSystem/model/Employee.java
@@ -1,4 +1,26 @@
 package com.example.EmployeePayrollSystem.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "salary", nullable = false)
+    private String salary;
 }
