daaafc2c1afd1f0f4aadf2e5082ef99801ade1b0
Prince <banerjee.prince69@gmail.com>
2024-08-25 02:32:22 +0530

    Employee Service

--- EmployeePayrollSystem/src/main/java/com/example/EmployeePayrollSystem/service/EmployeeService.java
+++ EmployeePayrollSystem/src/main/java/com/example/EmployeePayrollSystem/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.example.EmployeePayrollSystem.service;
 
+import com.example.EmployeePayrollSystem.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+    public Employee getEmployeeById(Long employeeId);
+    public Employee addEmployee(Employee employee);
+    public Employee updateEmployee(Long employeeId, Employee employee);
+    public void deleteEmployee(long employeeId);
 }
--- EmployeePayrollSystem/src/main/java/com/example/EmployeePayrollSystem/service/EmployeeServiceImpl.java
+++ EmployeePayrollSystem/src/main/java/com/example/EmployeePayrollSystem/service/EmployeeServiceImpl.java
@@ -1,4 +1,40 @@
 package com.example.EmployeePayrollSystem.service;
 
+import com.example.EmployeePayrollSystem.model.Employee;
+import com.example.EmployeePayrollSystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return List.of();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long employeeId) {
+        return null;
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(Long employeeId, Employee employee) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(long employeeId) {
+
+    }
 }
